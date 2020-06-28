8d21f77c06cfca37ae57ebbe35d81e151dfcd191
solankidhairya77 <solankidhairya77@gmail.com>
2020-06-27 22:18:39 -0400

    All done

--- index.php
+++ index.php
@@ -1,78 +1,93 @@
 <?php
   include('header.php');
 ?>
-<style>
-   body{
-        margin: 0px;
-        padding: 0px;
-        /* background: url('hi.jpg'); */
-        height: 100%;
-        background-repeat: no-repeat;
-        background-size: cover;
-        }
-</style>
 
 
-  <div id="demo" class="carousel slide" data-ride="carousel">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Agency - Start Bootstrap Theme</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-    <!-- Indicators -->
-    <ul class="carousel-indicators ">
-      <li data-target="#demo" data-slide-to="0" class="active"></li>
-      <li data-target="#demo" data-slide-to="1"></li>
-      <li data-target="#demo" data-slide-to="2"></li>
-      <li data-target="#demo" data-slide-to="3"></li>
-      <li data-target="#demo" data-slide-to="4"></li>
-      <li data-target="#demo" data-slide-to="5"></li>
-      <li data-target="#demo" data-slide-to="6"></li>
-      <li data-target="#demo" data-slide-to="7"></li>
-      <li data-target="#demo" data-slide-to="8"></li>
-      <li data-target="#demo" data-slide-to="9"></li>
-    </ul>
 
-    <!-- The slideshow -->
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="first.jpg" alt=""  style="height:495px; width:100%;"  >
-      </div>
-      <div class="carousel-item">
-        <img src="second.jpg" alt="" style="height:495px; width:100%;">
-      </div>
-      <div class="carousel-item">
-        <img src="third.jpg" alt="" style="height:495px; width:100%;"> 
-      </div>
-      <div class="carousel-item">
-        <img src="fourth.jpg" alt="" style="height:495px; width:100%;">
-      </div>
-      <div class="carousel-item">
-        <img src="hello.jpg" alt="" style="height:495px; width:100%;">
-      </div>
-      <div class="carousel-item">
-        <img src="fifth.jpg" alt="" style="height:495px; width:100%;">
-      </div>
-      <div class="carousel-item">
-        <img src="sixth.jpg" alt="" style="height:495px; width:100%;">
-      </div>
-      <div class="carousel-item">
-        <img src="seventh.jpg" alt="" style="height:495px; width:100%;">
-      </div>
-      <div class="carousel-item">
-        <img src="eight.jpg" alt="" style="height:495px; width:100%;">
-      </div>
-      <div class="carousel-item">
-        <img src="ninth.jpg" alt=""style="height:495px; width:100%;" >
-      </div>
-    </div>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+    <!-- <link href="css/pstyles.css" rel="stylesheet" /> -->
+    </head>
+    
+    <body>
+    <div id="demo" class="carousel slide" data-ride="carousel">
 
-    <!-- Left and right controls -->
-    <a class="carousel-control-prev" href="#demo" data-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-    </a>
-    <a class="carousel-control-next" href="#demo" data-slide="next">
-      <span class="carousel-control-next-icon"></span>
-    </a>
+<!-- Indicators -->
+<ul class="carousel-indicators ">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+  <li data-target="#demo" data-slide-to="3"></li>
+  <li data-target="#demo" data-slide-to="4"></li>
+  <li data-target="#demo" data-slide-to="5"></li>
+  <li data-target="#demo" data-slide-to="6"></li>
+  <li data-target="#demo" data-slide-to="7"></li>
+  <li data-target="#demo" data-slide-to="8"></li>
+  <li data-target="#demo" data-slide-to="9"></li>
+</ul>
+
+<!-- The slideshow -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="first.jpg" alt=""  style="height:495px; width:100%;"  >
+  </div>
+  <div class="carousel-item">
+    <img src="second.jpg" alt="" style="height:495px; width:100%;">
+  </div>
+  <div class="carousel-item">
+    <img src="third.jpg" alt="" style="height:495px; width:100%;"> 
+  </div>
+  <div class="carousel-item">
+    <img src="fourth.jpg" alt="" style="height:495px; width:100%;">
+  </div>
+  <div class="carousel-item">
+    <img src="hello.jpg" alt="" style="height:495px; width:100%;">
+  </div>
+  <div class="carousel-item">
+    <img src="fifth.jpg" alt="" style="height:495px; width:100%;">
+  </div>
+  <div class="carousel-item">
+    <img src="sixth.jpg" alt="" style="height:495px; width:100%;">
+  </div>
+  <div class="carousel-item">
+    <img src="seventh.jpg" alt="" style="height:495px; width:100%;">
+  </div>
+  <div class="carousel-item">
+    <img src="eight.jpg" alt="" style="height:495px; width:100%;">
+  </div>
+  <div class="carousel-item">
+    <img src="ninth.jpg" alt=""style="height:495px; width:100%;" >
+  </div>
 </div>
 
-    <!-- Bootstrap core JS-->
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+</div>
+
+  <!-- Bootstrap core JS-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
     <!-- Third party plugin JS-->
@@ -82,5 +97,6 @@
     <script src="assets/mail/contact_me.js"></script>
     <!-- Core theme JS-->
     <script src="js/scripts.js"></script>
-  </body>
+    </body>
+  
 </html>
